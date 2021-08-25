d907a275ddd78271ed624fc050b4b2c041b9edaa
Bennctu <physical31031@gmail.com>
2021-08-25 16:06:51 +0800

    add the loop in order to show the specific results

--- evaluate_traj.sh
+++ evaluate_traj.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-RESULT_DIR="../My_WorkSpace/vs_code_ws/src/lvio/ncrl_livins/demo/viral_results"
+TUM_PATH="../My_WorkSpace/vs_code_ws/src/lvio/ncrl_livins/demo/viral_results"
 NAME_list=("LiVINS" "RTLIO" "LIO_SAM" "FastLIO" "ALOAM" "VINS")
 
 GROUND_TRUTH="GroundTruth"
@@ -10,7 +10,6 @@ EVO_SETTING="--plot_relative_time --plot_mode=xy -p -a --t_max_diff 2.0"
 VIR_EEE="true"
 VIR_NYA="false"
 VIR_SBS="false"
-SHOW_ALL="false"
 
 
 # Assign EEE name list
@@ -18,19 +17,19 @@ if [[ "$VIR_EEE" == "true" ]]; then
 	declare -A EEE
 	for (( i = 0; i < ${#NAME_list[@]}; i++ )); do
 		for (( j = 1; j < 4; j++ )); do
-			EEE[$i,$j]=$RESULT_DIR/eee_0$j/${NAME_list[$i]}.tum
+			EEE[$i,$j]=$TUM_PATH/eee_0$j/${NAME_list[$i]}.tum
 			# echo ${EEE[$i,$j]}
 		done
 	done
 
-	echo "Choose the algorithm: 0:LiVINS; 1:RTLIO; 2:LIO_SAM; 3:FastLIO; 4:ALOAM; 5:VINS"
-	read CHOICE
-	for i in {1..3}; do
-		echo ---------------------------$i-th----------------------------------
-		evo_traj tum ${EEE[$CHOICE,$i]} --ref=$RESULT_DIR/eee_0$i/$GROUND_TRUTH.tum $EVO_SETTING
-		if [[ "$SHOW_ALL" == "true" ]]; then
-			evo_traj tum ${EEE[0,$i]} ${EEE[1,$i]} ${EEE[2,$i]} ${EEE[3,$i]} ${EEE[4,$i]} ${EEE[5,$i]} --ref=$RESULT_DIR/eee_0$i/$GROUND_TRUTH.tum $EVO_SETTING
-		fi
+	for j in {1..3}; do
+		echo $j"-th results"
+		declare COMPARE_RESULTS
+		for (( i = 0; i < ${#NAME_list[@]}; i++ )); do
+			COMPARE_RESULTS+=" ${EEE[$i,$j]}"
+		done
+		evo_traj tum $COMPARE_RESULTS --ref=$TUM_PATH/eee_0$j/$GROUND_TRUTH.tum $EVO_SETTING
+		COMPARE_RESULTS=""
 	done
 fi
 
@@ -39,19 +38,19 @@ if [[ "$VIR_NYA" == "true" ]]; then
 	declare -A NYA
 	for (( i = 0; i < ${#NAME_list[@]}; i++ )); do
 		for (( j = 1; j < 4; j++ )); do
-			NYA[$i,$j]=$RESULT_DIR/nya_0$j/${NAME_list[$i]}.tum
+			NYA[$i,$j]=$TUM_PATH/nya_0$j/${NAME_list[$i]}.tum
 			# echo ${NYA[$i,$j]}
 		done
 	done
 
-	echo "Choose the algorithm: 0:LiVINS; 1:RTLIO; 2:LIO_SAM; 3:FastLIO; 4:ALOAM; 5:VINS"
-	read CHOICE
-	for i in {1..3}; do
-		echo ---------------------------$i-th----------------------------------
-		evo_traj tum ${NYA[$CHOICE,$i]} --ref=$RESULT_DIR/nya_0$i/$GROUND_TRUTH.tum $EVO_SETTING
-		if [[ "$SHOW_ALL" == "true" ]]; then
-			evo_traj tum ${NYA[0,$i]} ${NYA[1,$i]} ${NYA[2,$i]} ${NYA[3,$i]} ${NYA[4,$i]} ${NYA[5,$i]} --ref=$RESULT_DIR/nya_0$i/$GROUND_TRUTH.tum $EVO_SETTING
-		fi
+	for j in {1..3}; do
+		echo $j"-th results"
+		declare COMPARE_RESULTS
+		for (( i = 0; i < ${#NAME_list[@]}; i++ )); do
+			COMPARE_RESULTS+=" ${NYA[$i,$j]}"
+		done
+		evo_traj tum $COMPARE_RESULTS --ref=$TUM_PATH/nya_0$j/$GROUND_TRUTH.tum $EVO_SETTING
+		COMPARE_RESULTS=""
 	done
 fi
 
@@ -60,25 +59,18 @@ if [[ "$VIR_SBS" == "true" ]]; then
 	declare -A SBS
 	for (( i = 0; i < ${#NAME_list[@]}; i++ )); do
 		for (( j = 1; j < 4; j++ )); do
-			SBS[$i,$j]=$RESULT_DIR/sbs_0$j/${NAME_list[$i]}.tum
+			SBS[$i,$j]=$TUM_PATH/sbs_0$j/${NAME_list[$i]}.tum
 			# echo ${SBS[$i,$j]}
 		done
 	done
 
-	echo "Choose the algorithm: 0:LiVINS; 1:RTLIO; 2:LIO_SAM; 3:FastLIO; 4:ALOAM; 5:VINS"
-	read CHOICE
-	for i in {1..3}; do
-		echo ---------------------------$i-th----------------------------------
-		evo_traj tum ${SBS[$CHOICE,$i]} --ref=$RESULT_DIR/sbs_0$i/$GROUND_TRUTH.tum $EVO_SETTING
-		if [[ "$SHOW_ALL" == "true" ]]; then
-			evo_traj tum ${SBS[0,$i]} ${SBS[1,$i]} ${SBS[2,$i]} ${SBS[3,$i]} ${SBS[4,$i]} ${SBS[5,$i]} --ref=$RESULT_DIR/sbs_0$i/$GROUND_TRUTH.tum $EVO_SETTING
-		fi
+	for j in {1..3}; do
+		echo $j"-th results"
+		declare COMPARE_RESULTS
+		for (( i = 0; i < ${#NAME_list[@]}; i++ )); do
+			COMPARE_RESULTS+=" ${SBS[$i,$j]}"
+		done
+		evo_traj tum $COMPARE_RESULTS --ref=$TUM_PATH/sbs_0$j/$GROUND_TRUTH.tum $EVO_SETTING
+		COMPARE_RESULTS=""
 	done
 fi
-
-
-# declare -a EEE[3]
-# for (( i = 1; i < 4; i++ )); do
-# 	EEE[$i]=$RESULT_DIR/eee_0$i/$NAME1.tum
-# 	echo ${EEE[$i]}
-# done
